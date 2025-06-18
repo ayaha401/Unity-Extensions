93425dbaf5a1339c6bc77978a99364ef7b2a7cef
ayaha401 <75297336+ayaha401@users.noreply.github.com>
2025-06-19 01:27:26 +0900

    Update Extensions.cs

--- Extensions.cs
+++ Extensions.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
 
 namespace AyahaGraphicComponents
 {
@@ -33,6 +35,31 @@ namespace AyahaGraphicComponents
             }
 
             return segments[index];
+        }
+
+        /// <summary>
+        /// 指定した名前を持つTransformを再帰的に探します。
+        /// </summary>
+        /// <param name="parent">検索を開始する親Transform</param>
+        /// <param name="targetName">探したいTransformの名前</param>
+        [CanBeNull]
+        public static Transform FindDeepChild(this Transform parent, string targetName)
+        {
+            if (parent.name == targetName)
+            {
+                return parent;
+            }
+
+            foreach (Transform child in parent)
+            {
+                Transform result = FindDeepChild(child, targetName);
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+
+            return null;
         }
     }
 }
